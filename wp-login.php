91e31e14a09a0941876e86d3763540ac00018258
kdesdarius <fellypejohnata@gmail.com>
2019-08-27 07:56:27 -0300

    testes

---
+++ wp-login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="pt-BR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-BR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Acessar &lsaquo; EME MATERIAIS PARA CONSTRUÇÃO &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='https://github.com/kdesdarius/siteeme/wp-includes/css/dashicons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://github.com/kdesdarius/siteeme/wp-includes/css/buttons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://github.com/kdesdarius/siteeme/wp-admin/css/forms.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://github.com/kdesdarius/siteeme/wp-admin/css/l10n.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://github.com/kdesdarius/siteeme/wp-admin/css/login.min.css?ver=5.2.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-pt-br">
+		<div id="login">
+		<h1><a href="https://br.wordpress.org/">Powered by WordPress</a></h1>
+	
+	<form name="loginform" id="loginform" action="https://github.com/kdesdarius/siteeme/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Nome de usuário ou endereço de e-mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Senha<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Lembrar-me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Acessar" />
+				<input type="hidden" name="redirect_to" value="https://github.com/kdesdarius/siteeme/wp-admin/" />
+					<input type="hidden" name="testcookie" value="1" />
+	</p>
+	</form>
+
+			<p id="nav">
+					<a href="https://github.com/kdesdarius/siteeme/minha-conta/lost-password/">Perdeu a senha?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
+
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
+
+			<p id="backtoblog"><a href="https://github.com/kdesdarius/siteeme/">
+		&larr; Voltar para EME MATERIAIS PARA CONSTRUÇÃO	</a></p>
+			
+	</div>
+
+	
+			<script type="text/javascript">
+			var wc_product_block_data = JSON.parse( decodeURIComponent( '%7B%22min_columns%22%3A1%2C%22max_columns%22%3A6%2C%22default_columns%22%3A3%2C%22min_rows%22%3A1%2C%22max_rows%22%3A6%2C%22default_rows%22%3A1%2C%22thumbnail_size%22%3A262%2C%22placeholderImgSrc%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fwordpress%5C%2Fwp-content%5C%2Fuploads%5C%2Fwoocommerce-placeholder-262x328.png%22%2C%22min_height%22%3A500%2C%22default_height%22%3A500%2C%22isLargeCatalog%22%3Afalse%2C%22limitTags%22%3Afalse%2C%22hasTags%22%3Afalse%2C%22productCategories%22%3A%5B%7B%22term_id%22%3A16%2C%22name%22%3A%22Sem%20Categoria%22%2C%22slug%22%3A%22sem-categoria%22%2C%22term_group%22%3A0%2C%22term_taxonomy_id%22%3A16%2C%22taxonomy%22%3A%22product_cat%22%2C%22description%22%3A%22%22%2C%22parent%22%3A0%2C%22count%22%3A0%2C%22filter%22%3A%22raw%22%2C%22link%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fwordpress%5C%2Fcategoria-produto%5C%2Fsem-categoria%5C%2F%22%7D%2C%7B%22term_id%22%3A19%2C%22name%22%3A%22Banheiro%22%2C%22slug%22%3A%22banheiro%22%2C%22term_group%22%3A0%2C%22term_taxonomy_id%22%3A19%2C%22taxonomy%22%3A%22product_cat%22%2C%22description%22%3A%22%22%2C%22parent%22%3A0%2C%22count%22%3A2%2C%22filter%22%3A%22raw%22%2C%22link%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fwordpress%5C%2Fcategoria-produto%5C%2Fbanheiro%5C%2F%22%7D%2C%7B%22term_id%22%3A18%2C%22name%22%3A%22Cimento%22%2C%22slug%22%3A%22cimento%22%2C%22term_group%22%3A0%2C%22term_taxonomy_id%22%3A18%2C%22taxonomy%22%3A%22product_cat%22%2C%22description%22%3A%22%22%2C%22parent%22%3A0%2C%22count%22%3A3%2C%22filter%22%3A%22raw%22%2C%22link%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fwordpress%5C%2Fcategoria-produto%5C%2Fcimento%5C%2F%22%7D%2C%7B%22term_id%22%3A20%2C%22name%22%3A%22Vaso%20Sanitario%22%2C%22slug%22%3A%22vaso-sanitario%22%2C%22term_group%22%3A0%2C%22term_taxonomy_id%22%3A20%2C%22taxonomy%22%3A%22product_cat%22%2C%22description%22%3A%22%22%2C%22parent%22%3A19%2C%22count%22%3A2%2C%22filter%22%3A%22raw%22%2C%22link%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fwordpress%5C%2Fcategoria-produto%5C%2Fbanheiro%5C%2Fvaso-sanitario%5C%2F%22%7D%5D%2C%22homeUrl%22%3A%22http%3A%5C%2F%5C%2Flocalhost%5C%2Fwordpress%5C%2F%22%7D' ) );
+		</script>
+			<div class="clear"></div>
+	</body>
+	</html>
+	
